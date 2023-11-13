9776da4c93f0a4a7308c4db2defdd2eeef125973
hrushikeshpthube <hrushikeshpthube@gmail.com>
2023-11-14 00:38:34 +0530

    show fulname(inpname) only after btn clicked

--- simpleclock/src/App.js
+++ simpleclock/src/App.js
@@ -2,9 +2,11 @@ import React, { useState } from "react";
 const App = () => {
   let newTime = new Date().toLocaleTimeString();
 
-  const [cTime, setCTime] = useState(newTime);
-  const [bg, setBg] = useState('red');
-  const [name, setName] = useState('Click me');
+  const [cTime, setCTime] = useState(newTime);//for time
+  const [bg, setBg] = useState('red');//for background change 
+  const [name, setName] = useState('Click me');//for button text change
+  const [inpname, setInpName] = useState("");//inputname for input field
+  const [fullName, setFullName] = useState();//to display name from input field only after submit button is clicked
 
 
   const UpdateTime = () => {
@@ -27,22 +29,40 @@ const App = () => {
 
   }
   const BgDoubleC = () => {
-   let newbg = 'red';
+    let newbg = 'red';
 
     setBg(newbg);
     setName("Double Clicked =red !!")
 
   }
 
+  const Display = (event) => {
+    // console.log(event.target.value);
+    setInpName(event.target.value);
+  }
+
+  const SubmitHandler = () => {
+    setFullName(inpname);
+
+  }
+
   return (
-    
+    <>
       <div style={{ backgroundColor: bg }}>
 
         <h1>{cTime}</h1>
         <button onClick={BgUpdate} onDoubleClick={BgDoubleC}>{name}</button>
       </div>
 
-   
+
+      <div>
+        <p>This is simple form without form tag</p>
+        <h1>Hello {fullName}</h1>
+        <input type="text" onChange={Display} />
+        <button onClick={SubmitHandler} >Submit</button>
+      </div>
+    </>
+
   )
 
 }
